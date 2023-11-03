79d7b65cab5f0e43188422c31d4fb99c3602cf10
Igor Todorovski <itodorov@ca.ibm.com>
2023-11-02 21:33:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+moreutils
+
+collection of the unix tools that nobody thought to write
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/moreutilsport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/moreutilsport.git'), string(name: 'PORT_DESCRIPTION', value: 'collection of the unix tools that nobody thought to write' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
